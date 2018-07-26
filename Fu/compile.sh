70e3a9bfa39cdc929561249dfef6fa6579ff6e34
guanjian-fu <921606998@qq.com>
2018-07-26 14:48:37 +0800

    update

--- Fu/compile.sh
+++ Fu/compile.sh
@@ -1,5 +1,5 @@
-# ifort -ipo -O3 -parallel -mkl -o FCIone FCIone.f
-# ifort -CU -CB -o gensps%tbme_simplepairing gensps%tbme_simplepairing.f
+ ifort -ipo -O3 -parallel -mkl -o FCIone FCIone.f
+ ifort -CU -CB -o gensps%tbme_simplepairing gensps%tbme_simplepairing.f
 # ifort -CU -CB -mkl -o CCone CCone.f
  ifort -ipo -O3 -parallel -mkl -o CCone CCone.f
 
