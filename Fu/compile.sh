079c8991a2c1b3cbf9ac01640f22bf52189240de
guanjian-fu <921606998@qq.com>
2018-07-31 21:00:25 +0800

    buiding nuclear matter

--- Fu/compile.sh
+++ Fu/compile.sh
@@ -1,6 +1,10 @@
- ifort -ipo -O3 -parallel -mkl -o FCIone FCIone.f
- ifort -CU -CB -o gensps%tbme_simplepairing gensps%tbme_simplepairing.f
-# ifort -CU -CB -mkl -o CCone CCone.f
- ifort -ipo -O3 -parallel -mkl -o CCone CCone.f
+clear
+
+ ifort -CU -CB -o gensps%tbme_nuclearmatter gensps%tbme_nuclearmatter.f
+ ifort -CU -CB -mkl -o CCone CCone.f
+
+# ifort -ipo -O3 -parallel -o gensps%tbme_simplepairing gensps%tbme_simplepairing.f
+# ifort -ipo -O3 -parallel -mkl -o FCIone FCIone.f
+# ifort -ipo -O3 -parallel -mkl -o CCone CCone.f
 
 rm *.mod
