b45e68a049141f4bbfa9a778479b402c60271376
guanjian-fu <921606998@qq.com>
2018-08-01 09:01:30 +0800

    Reorgnizing CCD nuclear matter

--- Fu/compile.sh
+++ Fu/compile.sh
@@ -1,10 +1,11 @@
 clear
 
- ifort -CU -CB -o gensps%tbme_nuclearmatter gensps%tbme_nuclearmatter.f
- ifort -CU -CB -mkl -o CCone CCone.f
+# ifort -CU -CB -o gensps%tbme_nuclearmatter gensps%tbme_nuclearmatter.f
+# ifort -CU -CB -mkl -o CCone CCone.f
 
 # ifort -ipo -O3 -parallel -o gensps%tbme_simplepairing gensps%tbme_simplepairing.f
+ ifort -ipo -O3 -parallel -o gensps%tbme_nuclearmatter gensps%tbme_nuclearmatter.f
 # ifort -ipo -O3 -parallel -mkl -o FCIone FCIone.f
-# ifort -ipo -O3 -parallel -mkl -o CCone CCone.f
+ ifort -ipo -O3 -parallel -mkl -o CCone CCone.f
 
 rm *.mod
